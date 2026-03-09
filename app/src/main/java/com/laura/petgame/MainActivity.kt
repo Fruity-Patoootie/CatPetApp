7437c11fc4234b9349c95c858168cb6a8402c970
Fruity-Patoootie <laura.b.kirkpatrick@gmail.com>
2026-03-09 10:16:20 -0500

    Mostly finished. Needs animations and Edit text

--- app/src/main/java/com/laura/petgame/MainActivity.kt
+++ app/src/main/java/com/laura/petgame/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.laura.petgame
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+private const val TAG = "PetMidterm"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,126 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Button declarations
+        val btnFeed = findViewById<Button>(R.id.btn_feed)
+        val btnPlay = findViewById<Button>(R.id.btn_play)
+        val btnPet = findViewById<Button>(R.id.btn_pet)
+        val btnVet = findViewById<Button>(R.id.btn_vet)
+
+        // Progress Bar declarations
+        val barHunger = findViewById<ProgressBar>(R.id.bar_hunger)
+        val barEnergy = findViewById<ProgressBar>(R.id.bar_energy)
+        val barHealth = findViewById<ProgressBar>(R.id.bar_health)
+        val barHappiness = findViewById<ProgressBar>(R.id.bar_happiness)
+
+
+        btnFeed.setOnClickListener {
+            barHunger.progress -= 5;
+            barHealth.progress -= 2;
+
+            Log.d("BUTTON_CLICK", "btnFeed clicked!")
+
+            // very long check statement to check win status
+            if (barHunger.progress <= 5 && barHealth.progress >= 90 && barEnergy.progress <= 10 && barHappiness.progress >= 90){
+                val intent = Intent(this@MainActivity, WinActivity::class.java)
+
+                intent.putExtra("health", barHealth.progress)
+                intent.putExtra("energy", barEnergy.progress)
+                intent.putExtra("happiness", barHappiness.progress)
+                intent.putExtra("hunger", barHunger.progress)
+
+                startActivity(intent)
+            }
+        }
+        // Play button click event
+        btnPlay.setOnClickListener {
+            barHunger.progress += 5;
+            barEnergy.progress -= 10;
+
+            Log.d("BUTTON_CLICK", "btnPlay clicked!")
+
+            // very long check statement to check win status
+            if (barHunger.progress <= 5 && barHealth.progress >= 90 && barEnergy.progress <= 10 && barHappiness.progress >= 90){
+                val intent = Intent(this@MainActivity, WinActivity::class.java)
+
+                intent.putExtra("health", barHealth.progress)
+                intent.putExtra("energy", barEnergy.progress)
+                intent.putExtra("happiness", barHappiness.progress)
+                intent.putExtra("hunger", barHunger.progress)
+
+                startActivity(intent)
+            }
+        }
+
+        // Vet button click event
+        btnVet.setOnClickListener {
+            barHappiness.progress -= 10;
+            barHealth.progress += 10;
+
+            Log.d("BUTTON_CLICK", "btnFeed clicked!")
+
+            // very long check statement to check win status
+            if (barHunger.progress <= 5 && barHealth.progress >= 90 && barEnergy.progress <= 10 && barHappiness.progress >= 90){
+                val intent = Intent(this@MainActivity, WinActivity::class.java)
+
+                intent.putExtra("health", barHealth.progress)
+                intent.putExtra("energy", barEnergy.progress)
+                intent.putExtra("happiness", barHappiness.progress)
+                intent.putExtra("hunger", barHunger.progress)
+
+                startActivity(intent)
+            }
+        }
+
+        btnPet.setOnClickListener {
+            barHappiness.progress += 10;
+            barEnergy.progress += 10;
+
+            Log.d("BUTTON_CLICK", "btnFeed clicked!")
+
+            // very long check statement to check win status
+            if (barHunger.progress <= 5 && barHealth.progress >= 90 && barEnergy.progress <= 10 && barHappiness.progress >= 90){
+                val intent = Intent(this@MainActivity, WinActivity::class.java)
+
+                intent.putExtra("health", barHealth.progress)
+                intent.putExtra("energy", barEnergy.progress)
+                intent.putExtra("happiness", barHappiness.progress)
+                intent.putExtra("hunger", barHunger.progress)
+
+                startActivity(intent)
+            }
+        }
+
     }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart → happens when Activity becomes visible")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume -> happens when Activity becomes visible")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause → happens when you press Home or open another Activity")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop → happens after Home or when Activity is fully hidden")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart → happens when returning from Recents after Home")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy → happens when Back is pressed or Activity is finishing (not guaranteed)")
+    }
+
 }
--- app/src/main/java/com/laura/petgame/WinActivity.kt
+++ app/src/main/java/com/laura/petgame/WinActivity.kt
@@ -1,6 +1,7 @@
 package com.laura.petgame
 
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,20 @@ class WinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_win)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val barHunger = findViewById<ProgressBar>(R.id.bar_hunger)
+        val barEnergy = findViewById<ProgressBar>(R.id.bar_energy)
+        val barHealth = findViewById<ProgressBar>(R.id.bar_health)
+        val barHappiness = findViewById<ProgressBar>(R.id.bar_happiness)
+
+        val hunger = intent.getIntExtra("hunger", 0)
+        val energy = intent.getIntExtra("energy", 0)
+        val health = intent.getIntExtra("health", 0)
+        val happiness = intent.getIntExtra("happiness", 0)
+
+        barHunger.progress = hunger
+        barEnergy.progress = energy
+        barHealth.progress = health
+        barHappiness.progress = happiness
     }
 }
